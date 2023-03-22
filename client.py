8b11b199290814e947fafd9498e81da0254e7f21
Josué Romero <79048209+josuerom@users.noreply.github.com>
2023-03-22 00:55:05 -0500

    Actividad No. 3 - Sistemas Distribuidos

---
+++ client.py
@@ -0,0 +1,27 @@
+import socket
+
+def client_program():
+    #host = socket.gethostname()  # as both code is running on same pc
+    host = "172.18.0.2"
+    port = 5000  # socket server port number
+
+    client_socket = socket.socket()  # instantiate
+    client_socket.connect((host, port))  # connect to the server
+
+    username = input("Ingrese el nombre: ")  # take input
+    message = input("--> ")
+
+    while message.lower().strip() != 'bye':
+        client_socket.send(message.encode())  # send message
+        data = client_socket.recv(1024).decode()  # receive response
+
+        print('Repuesta del servidor: ' + data)  # show in terminal
+        message = input("--> ")  # again take input
+
+    client_socket.close()  # close the connection
+    
+    f = open('output.txt', 'w')
+    f.write('Nombre del cliente: ' + username + '\n')
+
+if __name__ == '__main__':
+    client_program()
---
+++ server.py
@@ -0,0 +1,31 @@
+import socket
+
+def server_program():
+    # get the hostname
+    host = "172.18.0.2"
+    port = 5000  # initiate port no above 1024
+
+    server_socket = socket.socket()  # get instance
+    server_socket.bind((host, port))  # bind host address and port together
+
+    # configure how many client the server can listen simultaneously
+    server_socket.listen(2)
+    conn, address = server_socket.accept()  # accept new connection
+    print("Se ha establecido conexion con el cliente: " + str(address))
+    while True:
+        # receive data stream. it won't accept data packet greater than 1024 bytes
+        data = conn.recv(1024).decode()
+        if not data:
+            break
+        print("Respuesta del cliente: " + str(data))
+        data = input('--> ')
+        conn.send(data.encode())  # send data to the client
+
+    conn.close()  # close the connection
+
+    f = open('output.txt', 'a')
+    f.write('IP: ' + str(address) + '\n')
+    f.close()
+
+if __name__ == '__main__':
+    server_program()
